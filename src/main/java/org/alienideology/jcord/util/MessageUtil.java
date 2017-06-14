fe8d245f69e68b55dd74fb50a8723992776ef6ef
AlienIdeology <wcliaw610@gmail.com>
2017-06-13 22:11:27 -0500

    Fix Patterns, thx to Xaaaaaaaanit

--- src/main/java/org/alienideology/jcord/util/MessageUtil.java
+++ src/main/java/org/alienideology/jcord/util/MessageUtil.java
@@ -8,11 +8,11 @@ import java.util.regex.Pattern;
  */
 public class MessageUtil {
 
-    public static Pattern PATTERN_MENTION_USER = Pattern.compile("(?=<@)(<@[0-9]+?>)(?<=>)");
-    public static Pattern PATTERN_MENTION_MEMBER = Pattern.compile("(?=<@!)(<@![0-9]+?>)(?<=>)");
-    public static Pattern PATTERN_MENTION_CHANNEL = Pattern.compile("(?=<#)(<#[0-9]+?>)(?<=>)");
-    public static Pattern PATTERN_MENTION_ROLE = Pattern.compile("(?=<@&)(<@&[0-9]+?>)(?<=>)");
-    public static Pattern PATTERN_MENTION_EMOJI = Pattern.compile("(?=<:)(<:)((\\w|[ ]).?)(:)([0-9]+?)(>)(?<=>)");
+    public static Pattern PATTERN_MENTION_USER = Pattern.compile("(<@[0-9]+?>)");
+    public static Pattern PATTERN_MENTION_MEMBER = Pattern.compile("(<@![0-9]+?>)");
+    public static Pattern PATTERN_MENTION_CHANNEL = Pattern.compile("(<#[0-9]+?>)");
+    public static Pattern PATTERN_MENTION_ROLE = Pattern.compile("(<@&[0-9]+?>)");
+    public static Pattern PATTERN_MENTION_EMOJI = Pattern.compile("(<:)((\\w|[ ]).?)(:)([0-9]+?)(>)");
 
     public static Pattern PATTERN_IS_URL = Pattern.compile("(https|http|attachment)(://)(.+[^\\s])([.])(.+[^\\s])");
 
