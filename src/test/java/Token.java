63257fcd5fe26f20f4b9c05afde1674c4848cead
Allen Liao <wcliaw610@gmail.com>
2017-06-22 12:27:35 -0500

    Maaaa Tooookeeeen

--- src/test/java/Token.java
+++
@@ -1,8 +0,0 @@
-/**
- * @author AlienIdeology
- */
-public class Token {
-
-    public final static String YOUR_SECRET_TOKEN_HERE = "MzIwMDAyOTQ2NTEzMjQwMDcx.DClgcQ.M4OHnMrR0OstFcb7_jETX4V2AoA";
-
-}
