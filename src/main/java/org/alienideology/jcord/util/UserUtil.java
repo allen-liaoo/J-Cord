bd73804803dadda85daae627a45fb7ca5ae2e2c8
xaanit <shadowjacob@yahoo.com>
2017-07-06 13:51:32 -0500

    Added not allowed usernames.

--- src/main/java/org/alienideology/jcord/util/UserUtil.java
+++ src/main/java/org/alienideology/jcord/util/UserUtil.java
@@ -8,9 +8,10 @@ public class UserUtil {
     private static final int MIN_USERNAME_LENGTH = 2;
     private static final int MAX_USERNAME_LENGTH = 32;
     private static final List<Character> DISSALOWED_CHARS = Arrays.asList('@', '`', ':', '#');
+    private static final List<String> DISSALOWED_NAMES = Arrays.asList("DISCORDTAG", "EVERYONE", "HERE");
 
     public static boolean validUsername(String str) {
-        return str.length() >= MIN_USERNAME_LENGTH && str.length() <= MAX_USERNAME_LENGTH && !contains(str, DISSALOWED_CHARS);
+        return str.length() >= MIN_USERNAME_LENGTH && str.length() <= MAX_USERNAME_LENGTH && !contains(str, DISSALOWED_CHARS) && DISSALOWED_NAMES.contains(str.trim().toUpperCase());
     }
 
     private static boolean contains(String str, List<Character> arg) {
